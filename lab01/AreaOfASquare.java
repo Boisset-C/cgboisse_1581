e8081e8a8d557bf73eef6dabdbd5133bb2b4a5cf
cosme boisset <boissetcosme@gmail.com>
2024-08-29 13:42:41 -0500

    CS 1581: Lab01 - Finished AreaOfSquares and MinutesToSeconds Classes. WIP.

---
+++ lab01/AreaOfASquare.java
@@ -0,0 +1,21 @@
+//Cosme Boisset - Problem 1: AreaOfASquare
+import java.util.Scanner;
+
+public class AreaOfASquare {
+	//method
+	public static void main(String[] args) {
+		//prompt user to input a length to calculate Area of square
+		System.out.print("Input a number to calculate the Area of a Square: ");
+		
+		// create scanner class to store user input
+		Scanner input = new Scanner(System.in);
+
+		//declare length variable to store user input 
+		int length = input.nextInt();
+		//declare area variable to store result of computation 
+		int area = length * length;
+		//console log result
+		System.out.println(area);
+	}
+
+}
---
+++ lab01/MinutesToSeconds.java
@@ -0,0 +1,21 @@
+//Cosme Boisset - Problem 2: MinutesToSeconds
+import java.util.Scanner;
+
+public class MinutesToSeconds {
+	public static void main(String[] args) {
+		//prompt user to input a number of minutes to be calculated into seconds
+		System.out.print("Please input a number of minutes to calculate into seconds: ");
+
+		//create scanner class to store user input
+		Scanner input = new Scanner(System.in);
+
+		//create variable to store user input
+		int userInputMinutes = input.nextInt();
+		//create variable to store result
+		int resultOfMinutestoSeconds = userInputMinutes * 60;
+
+		//console log result aka output
+		System.out.println(resultOfMinutestoSeconds);
+
+	}
+}
